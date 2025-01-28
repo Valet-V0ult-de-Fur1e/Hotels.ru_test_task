3c9da9ecfdd8a150fd677aae335a29bd03b91f0b
Egor Mironov <egor.a.mironov@gmail.com>
2025-01-28 20:17:41 +0300

    deleted python code

--- task1.py
+++
@@ -1,9 +0,0 @@
-def task1(number:int)->str:
-    computer_word_end = ""
-    if number % 10 == 1 and number % 100 != 11:
-       computer_word_end = ""
-    elif 2 <= number % 10 <= 4 and not (12 <= number % 100 <= 14):
-       computer_word_end = "а"
-    else:
-       computer_word_end = "ов"
-    return f"{number} компьютер{computer_word_end}"
--- task3.py
+++
@@ -1,7 +0,0 @@
-def task3(start_number:int, end_number:int)->list:
-    def is_prime(num):
-        for div in range(2, int(num ** 0.5) + 1):
-            if num % div == 0:
-                return False
-        return True
-    return [number for number in range(start_number, end_number + 1) if number > 0 and is_prime(number)]
